825afa13c4814ed19fb0fe98bf07517bddf8dba3
Frannsiny <fcerb317@mtroyal.ca>
2025-10-01 16:08:13 -0600

    Update HPCsignin.sh

--- HPCsignin.sh
+++ HPCsignin.sh
@@ -14,3 +14,19 @@ sftp francine@fir.computecanada.ca
 
 # `put <file>` copies file from computer to HPC
 # `get <file>` copies file from HPC to computer
+
+# create environment with vcf tool in it 
+conda create -n vcftools -c bioconda vcftools
+
+# output:
+# To activate this environment, use
+#
+#     $ conda activate vcftools
+#
+# To deactivate an active environment, use
+#
+#     $ conda deactivate
+
+# Note the difference base vs vcftools
+(base) [francine@login2 ~]$ conda activate vcftools
+(vcftools) [francine@login2 ~]$
